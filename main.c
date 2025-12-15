662d992629ba84ee388f5e43865b33809a4760b8
Hamid Adesokan <aadehamid@gmail.com>
2025-12-15 04:20:27 +0000

    Build the program

--- main.c
+++ main.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 //
 // Created by Hamid Adesokan on 12/14/25.
 //
@@ -5,4 +6,10 @@
 int main()
 {
    printf("Hello, World!\n");
+=======
+#include <stdio.h>
+int main(){
+    printf("Hello, World!\n");
+    return 0;
+>>>>>>> 5a5297f (Create CMakeLists.txt and ran the main.c file)
 }
