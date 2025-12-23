b033d4a910df841efc47d0e2bb402d3e353a9d94
Hamid Adesokan <aadehamid@gmail.com>
2025-12-23 12:45:23 -0600

    modified file

--- main.c
+++ main.c
@@ -1,15 +1,9 @@
-<<<<<<< HEAD
 //
 // Created by Hamid Adesokan on 12/14/25.
 //
 #include <stdio.h>
-int main()
-{
-   printf("Hello, World!\n");
-=======
-#include <stdio.h>
-int main(){
+
+int main() {
     printf("Hello, World!\n");
     return 0;
->>>>>>> 5a5297f (Create CMakeLists.txt and ran the main.c file)
 }
